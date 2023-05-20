820a00a8b47fea7f7cfff2617d0ff4f8c7c981e7
Cheng Yuan <84340069+chengyuan0124@users.noreply.github.com>
2023-05-19 23:14:02 -0400

    Updated docker build process

--- docker/install.sh
+++ docker/install.sh
@@ -14,8 +14,59 @@
 #  License for the specific language governing permissions and limitations under
 #  the License.
 
-source /opt/ros/kinetic/setup.bash
-cd ~/
-colcon build  --cmake-args -DCMAKE_BUILD_TYPE=Release
+sudo add-apt-repository ppa:deadsnakes/ppa -y
 
-chmod -R ugo+x ~/install
+sudo apt-get update && sudo apt-get install -y --no-install-recommends \
+                            python3.7 \
+                            python3.7-distutils \
+                            python3-pip \
+                            python3-wheel \
+                            python3 \
+                            python3-numpy \
+                            libgps-dev \
+                            ros-noetic-ackermann-msgs \
+                            ros-noetic-derived-object-msgs \
+                            ros-noetic-jsk-recognition-msgs \
+                            ros-noetic-rqt \
+                            ros-noetic-rviz \
+                            wget
+
+
+python3.7 -m pip install simple-pid
+python3.7 -m pip install numpy --upgrade
+update-alternatives --install /usr/bin/python python /usr/bin/python3 10
+alias python='/usr/bin/python3.7'
+
+# Clone CARLA ROS bridge
+sudo git clone --depth 1 -b '0.9.10.1' --recurse-submodules https://github.com/carla-simulator/ros-bridge.git
+
+# Clone ROS message
+mkdir -p ~/msgs
+cd ~/msgs && sudo git clone --depth 1 --single-branch -b ${CARMA_VERSION} https://github.com/usdot-fhwa-stol/autoware.ai.git
+cd ~/msgs && sudo git clone --depth 1 --single-branch -b ${CARMA_VERSION} https://github.com/usdot-fhwa-stol/carma-msgs.git
+
+# CARMA Utils package
+mkdir -p ~/utils
+cd ~/utils && sudo git clone --depth 1 --single-branch -b ${CARMA_VERSION} https://github.com/usdot-fhwa-stol/carma-utils.git
+
+# GPS Common
+mkdir -p ~/gps && cd ~/gps 
+sudo git clone https://github.com/swri-robotics/gps_umd.git
+
+mkdir -p ~/carma_carla_ws/src/msgs && cd ~/carma_carla_ws/src/msgs
+
+ln -s ~/msgs/carma-msgs/j2735_msgs
+ln -s ~/msgs/carma-msgs/cav_msgs
+ln -s ~/msgs/carma-msgs/can_msgs
+ln -s ~/msgs/carma-msgs/cav_srvs
+ln -s ~/msgs/carma-msgs/carma_cmake_common
+ln -s ~/msgs/autoware.ai/messages/autoware_msgs
+
+mkdir -p ~/carma_carla_ws/src/utils && cd ~/carma_carla_ws/src/utils 
+ln -s ~/utils/carma-utils/carma_utils
+
+cd ~/carma_carla_ws/src && 
+ln -s ~/ros-bridge
+ln -s ~/carma-carla-integration
+
+cd ~/carma_carla_ws && /bin/bash -c '. /opt/ros/noetic/setup.bash; catkin_make'
