db4550af7d35056386bfe8c336031c958c3f0528
dev <paul.k.bourelly@leidos.com>
2023-12-28 10:22:01 -0500

    Updated script with arg parser

--- carma-carla-integration/carla-health-poll/src/wait_for_carla.py
+++ carma-carla-integration/carla-health-poll/src/wait_for_carla.py
@@ -1,14 +1,35 @@
 import carla
 import logging
+import argparse
+
 logging.getLogger().setLevel(logging.INFO)
 connected = False
+arg_parser = argparse.ArgumentParser(description=__doc__)
+arg_parser.add_argument(
+        "--carla-host",
+        default="172.2.0.2",
+        type=str,
+        help="CARLA host. (default: \"localhost\")")
+
+arg_parser.add_argument(
+    "--carla-port",
+    default=2000,
+    type=int,
+    help="CARLA host. (default: 2000)")
+arg_parser.add_argument(
+    "--carla-timeout",
+    default=10,
+    type=int,
+    help="CARLA Client timeout in seconds. (default: 10)")
+args = arg_parser.parse_args()
+
 while not connected:
     try:
-        logging.info("Attempting to connect to CARLA at 172.2.0.2:2000")
+        logging.info(f"Attempting to connect to CARLA at {args.carla_host}:{args.carla_port}" )
         carla_client = carla.Client(
-                "172.2.0.2",
-                2000)
-        carla_client.set_timeout(10)
+                args.carla_host,
+                args.carla_port)
+        carla_client.set_timeout(args.carla_timeout)
         carla_world = carla_client.get_world()
         connected = True
     except (IOError, RuntimeError) as e:
