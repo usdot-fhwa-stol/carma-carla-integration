a345fb4fde4d867f49fafeb5dce653293233089c
dev <paul.k.bourelly@leidos.com>
2023-12-28 12:14:51 -0500

    Add description documentation

--- carma-carla-integration/carla-health-poll/src/wait_for_carla.py
+++ carma-carla-integration/carla-health-poll/src/wait_for_carla.py
@@ -1,10 +1,42 @@
+# Copyright (C) 2023 LEIDOS.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may not
+# use this file except in compliance with the License. You may obtain a copy of
+# the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations under
+# the License.
 import carla
 import logging
 import argparse
 
+def wait_for_carla(args):
+    """
+    This is a simple python script to standup a CARLA client that only terminates onces a successful connection to CARLA is established. This avoids timeout issues with the CARLA Ros Bridge described here (https://github.com/usdot-fhwa-stol/carma-carla-integration/issues/65). This script can simply be called as a precursor to CARLA depedent services to ensure that CARLA is healthy before launching.
+    """
+    connected = False
+    
+    while not connected:
+        try:
+            logging.info(f"Attempting to connect to CARLA at {args.carla_host}:{args.carla_port}" )
+            carla_client = carla.Client(
+                    args.carla_host,
+                    args.carla_port)
+            carla_client.set_timeout(args.carla_timeout)
+            carla_world = carla_client.get_world()
+            connected = True
+        except (IOError, RuntimeError) as e:
+            logging.warning("Error connecting to carla: {}".format(e))
+        except KeyboardInterrupt:
+            pass
+    logging.info("CARLA Connection successful ... ")
 logging.getLogger().setLevel(logging.INFO)
-connected = False
-arg_parser = argparse.ArgumentParser(description=__doc__)
+arg_parser = argparse.ArgumentParser(description=wait_for_carla.__doc__)
 arg_parser.add_argument(
         "--carla-host",
         default="172.2.0.2",
@@ -22,18 +54,4 @@ arg_parser.add_argument(
     type=int,
     help="CARLA Client timeout in seconds. (default: 10)")
 args = arg_parser.parse_args()
-
-while not connected:
-    try:
-        logging.info(f"Attempting to connect to CARLA at {args.carla_host}:{args.carla_port}" )
-        carla_client = carla.Client(
-                args.carla_host,
-                args.carla_port)
-        carla_client.set_timeout(args.carla_timeout)
-        carla_world = carla_client.get_world()
-        connected = True
-    except (IOError, RuntimeError) as e:
-        logging.warning("Error connecting to carla: {}".format(e))
-    except KeyboardInterrupt:
-        pass
-logging.info("CARLA Connection successful ... ")
+wait_for_carla(args)
