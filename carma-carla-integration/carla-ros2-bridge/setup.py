b36e094175390e36eabd9139b21ed8fb0e49a96c
Will Varner <willv678@gmail.com>
2025-06-16 17:25:27 -0400

    Trimmed for only core shared files between for main bridge functionality

--- carma-carla-integration/carla-ros2-bridge/setup.py
+++ carma-carla-integration/carla-ros2-bridge/setup.py
@@ -1,4 +1,4 @@
-from setuptools import find_packages, setup
+from setuptools import setup, find_packages
 import os
 from glob import glob
 
@@ -11,12 +11,9 @@ setup(
     package_dir={'': 'src'},
     
     data_files=[
-        # Install marker file for ament resource index
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]), 
-        # Install package.xml
         ('share/' + package_name, ['package.xml']),
-        # Install all .launch.py files from the 'launch' directory
         (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*.launch.py'))),
     ],
     install_requires=['setuptools'],
@@ -25,12 +22,11 @@ setup(
     maintainer_email='will.varner@uga.edu',
     description='Ported CARLA ROS 2 bridge for CDASim.',
     license='MIT',
-    tests_require=['pytest'],
     
     entry_points={
         'console_scripts': [
+            # This base PR only provides the main bridge executable
             'bridge = carla_ros2_bridge.bridge:main',
-            'ackermann_control = carla_ros2_bridge.ackermann_control.carla_ackermann_control_node:main',
         ],
     },
 )
