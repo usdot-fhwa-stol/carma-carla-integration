a1f2d050ec30903fdae4dab7dfed583d0a93efa8
west053 <rwf88570@uga.edu>
2025-07-01 17:16:06 -0400

    Corrected launch file header

--- carma-carla-integration/carla-ros2-bridge/launch/carla_ros2_bridge.launch.py
+++ carma-carla-integration/carla-ros2-bridge/launch/carla_ros2_bridge.launch.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # Copyright (C) 2021 LEIDOS.
-# Developped by Ryan Fleming @ UGA MSC Lab 2025
+# Developed by Ryan Fleming @ UGA MSC Lab 2025
 #
 # Licensed under the Apache License, Version 2.0 (the "License"); you may not
 # use this file except in compliance with the License. You may obtain a copy of
