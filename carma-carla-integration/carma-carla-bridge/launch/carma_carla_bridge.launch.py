1e877d23ed8f42fdd7a1371c9f67e356056043b0
west053 <rwf88570@uga.edu>
2025-07-23 19:24:27 -0400

    Update to launch args

--- carma-carla-integration/carma-carla-bridge/launch/carma_carla_bridge.launch.py
+++ carma-carla-integration/carma-carla-bridge/launch/carma_carla_bridge.launch.py
@@ -6,26 +6,83 @@ from launch_ros.actions import Node
 
 def generate_launch_description():
     return LaunchDescription([
-        # Launch args
-        DeclareLaunchArgument('host', default_value='localhost'),
-        DeclareLaunchArgument('port', default_value='2000'),
-        DeclareLaunchArgument('timeout', default_value='10.0'),
-        DeclareLaunchArgument('passive', default_value='False'),
-        DeclareLaunchArgument('synchronous_mode', default_value='True'),
-        DeclareLaunchArgument('synchronous_mode_wait_for_vehicle_control_command', default_value='false'),
-        DeclareLaunchArgument('fixed_delta_seconds', default_value='0.05'),
-        DeclareLaunchArgument('town', default_value='Town04'),
-        DeclareLaunchArgument('role_name', default_value='carma_1'),
-        DeclareLaunchArgument('vehicle_filter', default_value='vehicle.toyota.prius'),
-        DeclareLaunchArgument('spawn_point', default_value='15.4,-90.1,0,0,0,90'),
-        DeclareLaunchArgument('launch_spawn_vehicle', default_value='true'),
-        DeclareLaunchArgument('launch_ackermann_control', default_value='true'),
-        DeclareLaunchArgument('hero_config_path', default_value=PathJoinSubstitution([
-            FindPackageShare('carla_ros2_bridge'), 'configs', 'carma_hero_stack.json'])),
-        DeclareLaunchArgument('speed_Kp', default_value='0.4'),
-        DeclareLaunchArgument('speed_Ki', default_value='0.03'),
-        DeclareLaunchArgument('speed_Kd', default_value='0.0'),
-        DeclareLaunchArgument('accel_Kp', default_value='0.05'),
-        DeclareLaunchArgument('accel_Ki', default_value='0.0'),
-        DeclareLaunchArgument('accel_Kd', default_value='0.05'),
+        # Declare arguments
+        DeclareLaunchArgument(
+            name='host',
+            default_value='localhost',
+            description='IP of the CARLA server'
+        ),
+        DeclareLaunchArgument(
+            name='port',
+            default_value='2000',
+            description='TCP port of the CARLA server'
+        ),
+        DeclareLaunchArgument(
+            name='timeout',
+            default_value='10.0',
+            description='Time to wait for a successful connection to the CARLA server'
+        ),
+        DeclareLaunchArgument(
+            name='passive',
+            default_value='False',
+            description='When enabled, the ROS bridge will take a backseat and another client must tick the world (only in synchronous mode)'
+        ),
+        DeclareLaunchArgument(
+            name='synchronous_mode',
+            default_value='True',
+            description='Enable/disable synchronous mode. If enabled, the ROS bridge waits until the expected data is received for all sensors'
+        ),
+        DeclareLaunchArgument(
+            name='synchronous_mode_wait_for_vehicle_control_command',
+            default_value='false',
+            description='When enabled, pauses the tick until a vehicle control is completed (only in synchronous mode)'
+        ),
+        DeclareLaunchArgument(
+            name='fixed_delta_seconds',
+            default_value='0.05',
+            description='Simulation time (delta seconds) between simulation steps'
+        ),
+        DeclareLaunchArgument(
+            name='town',
+            default_value='Town01',
+            description='Either use an available CARLA town (eg. "Town01") or an OpenDRIVE file (ending in .xodr)'
+        ),
+        DeclareLaunchArgument(
+            name='role_name',
+            default_value='hero',
+            description='Role name to identify ego vehicle, should match role_name in config at hero_config_path'
+        ),
+        DeclareLaunchArgument(
+            name='vehicle_filter',
+            default_value='vehicle.*',
+            description='Selects which vehicles are availiable in CARLA for spawn'
+        ),
+        DeclareLaunchArgument(
+            name='spawn_point',
+            default_value='None',
+            description='Spawn point to be used for vehicle spawn in CARLA'
+        ),
+        DeclareLaunchArgument(
+            name='launch_spawn_vehicle',
+            default_value='true',
+            description='Determines if spawn_hero_vehicle script is launched alongside carla-ros2-bridge node'
+        ),
+        DeclareLaunchArgument(
+            name='launch_ackermann_control',
+            default_value='false',
+            description='Determines if ackermann control node is launched alongside carla-ros2-bridge node'
+        ),
+        DeclareLaunchArgument(
+            name='hero_config_path',
+            default_value=PathJoinSubstitution([
+                FindPackageShare('carla_ros2_bridge'),
+                'configs',
+                'stack.json'
+            ]),
+            description='Path to the hero vehicle JSON config'
+        ),
+
+
+        
+
     ])
