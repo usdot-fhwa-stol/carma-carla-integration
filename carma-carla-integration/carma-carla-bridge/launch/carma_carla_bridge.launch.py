2674a92bb6822a60b13a147a4ff05328064447b3
west053 <rwf88570@uga.edu>
2025-07-23 16:38:17 -0400

    Added launch args

--- carma-carla-integration/carma-carla-bridge/launch/carma_carla_bridge.launch.py
+++ carma-carla-integration/carma-carla-bridge/launch/carma_carla_bridge.launch.py
@@ -1,6 +1,26 @@
 from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
 from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
 
 def generate_launch_description():
-    return LaunchDescription([])
+    return LaunchDescription([
+        # Launch args
+        DeclareLaunchArgument('host', default_value='localhost'),
+        DeclareLaunchArgument('port', default_value='2000'),
+        DeclareLaunchArgument('vehicle_filter', default_value='vehicle.toyota.prius'),
+        DeclareLaunchArgument('role_name', default_value='ego_vehicle'),
+        DeclareLaunchArgument('spawn_point', default_value=''),
+
+        DeclareLaunchArgument('speed_Kp', default_value='0.05'),
+        DeclareLaunchArgument('speed_Ki', default_value='0.0'),
+        DeclareLaunchArgument('speed_Kd', default_value='0.5'),
+
+        DeclareLaunchArgument('accel_Kp', default_value='0.05'),
+        DeclareLaunchArgument('accel_Ki', default_value='0.0'),
+        DeclareLaunchArgument('accel_Kd', default_value='0.05'),
+
+        DeclareLaunchArgument('synchronous_mode', default_value='false'),
+        DeclareLaunchArgument('synchronous_mode_wait_for_vehicle_control_command', default_value='false'),
+        DeclareLaunchArgument('fixed_delta_seconds', default_value='0.05'),
+    ])
