0c3f1b2668a14563d8b42d7525734955a3aa9b60
west053 <rwf88570@uga.edu>
2025-07-29 19:24:19 -0400

    Updated build-image.sh

--- docker/build-image.sh
+++ docker/build-image.sh
@@ -14,36 +14,34 @@
 #  License for the specific language governing permissions and limitations under
 #  the License.
 
+#!/bin/bash
+set -e
 USERNAME=usdotfhwastol
 
 cd "$(dirname "$0")"
 IMAGE=$(./get-image-name.sh | tr '[:upper:]' '[:lower:]')
 
 echo ""
-echo "##### $IMAGE Docker Image Build Script #####"
+echo "##### $IMAGE ROS 2 Docker Image Build Script #####"
 echo ""
 
 while [[ $# -gt 0 ]]; do
-    arg="$1"
-    case $arg in
+    case $1 in
         -v|--version)
             COMPONENT_VERSION_STRING="$2"
-            shift
-            shift
-            ;;
+            shift; shift ;;
         --system-release)
             SYSTEM_RELEASE=true
-            shift
-            ;;
+            shift ;;
         -p|--push)
             PUSH=true
-            shift
-            ;;
+            shift ;;
         -d|--develop)
             USERNAME=usdotfhwastoldev
-            COMPONENT_VERSION_STRING=develop
-            shift
-            ;;
+            COMPONENT_VERSION_STRING=develop-ros2
+            shift ;;
+        *)
+            shift ;;
     esac
 done
 
@@ -51,21 +49,21 @@ if [[ -z "$COMPONENT_VERSION_STRING" ]]; then
     COMPONENT_VERSION_STRING=$("./get-component-version.sh")
 fi
 
-echo "Building docker image for $IMAGE version: $COMPONENT_VERSION_STRING"
+echo "Building ROS 2 docker image for $IMAGE version: $COMPONENT_VERSION_STRING"
 echo "Final image name: $USERNAME/$IMAGE:$COMPONENT_VERSION_STRING"
 
 cd ..
-if [[ $COMPONENT_VERSION_STRING = "develop" ]]; then
+if [[ $COMPONENT_VERSION_STRING = "develop-ros2" ]]; then
     sed "s|usdotfhwastoldev/|$USERNAME/|g; s|usdotfhwastolcandidate/|$USERNAME/|g; s|usdotfhwastol/|$USERNAME/|g; s|:[0-9]*\.[0-9]*\.[0-9]*|:$COMPONENT_VERSION_STRING|g; s|checkout.sh|checkout.sh -d|g" \
         Dockerfile | docker build -f - --no-cache -t $USERNAME/$IMAGE:$COMPONENT_VERSION_STRING \
         --build-arg VERSION="$COMPONENT_VERSION_STRING" \
-        --build-arg VCS_REF=`git rev-parse --short HEAD` \
-        --build-arg BUILD_DATE=`date -u +”%Y-%m-%dT%H:%M:%SZ”` .
+        --build-arg VCS_REF=$(git rev-parse --short HEAD) \
+        --build-arg BUILD_DATE=$(date -u +"%Y-%m-%dT%H:%M:%SZ") .
 else
     docker build --no-cache -t $USERNAME/$IMAGE:$COMPONENT_VERSION_STRING \
         --build-arg VERSION="$COMPONENT_VERSION_STRING" \
-        --build-arg VCS_REF=`git rev-parse --short HEAD` \
-        --build-arg BUILD_DATE=`date -u +”%Y-%m-%dT%H:%M:%SZ”` .
+        --build-arg VCS_REF=$(git rev-parse --short HEAD) \
+        --build-arg BUILD_DATE=$(date -u +"%Y-%m-%dT%H:%M:%SZ") .
 fi
 
 TAGS=()
@@ -73,7 +71,6 @@ TAGS+=("$USERNAME/$IMAGE:$COMPONENT_VERSION_STRING")
 
 docker tag $USERNAME/$IMAGE:$COMPONENT_VERSION_STRING $USERNAME/$IMAGE:latest
 TAGS+=("$USERNAME/$IMAGE:latest")
-
 echo "Tagged $USERNAME/$IMAGE:$COMPONENT_VERSION_STRING as $USERNAME/$IMAGE:latest"
 
 if [ "$SYSTEM_RELEASE" = true ]; then
@@ -84,10 +81,10 @@ if [ "$SYSTEM_RELEASE" = true ]; then
 fi
 
 if [ "$PUSH" = true ]; then
-    for tag in $TAGS; do
-        docker push "${tag}"
+    for tag in "${TAGS[@]}"; do
+        docker push "$tag"
     done
 fi
 
 echo ""
-echo "##### $IMAGE Docker Image Build Done! #####"
+echo "##### ROS 2 Docker Image Build Complete! #####"
