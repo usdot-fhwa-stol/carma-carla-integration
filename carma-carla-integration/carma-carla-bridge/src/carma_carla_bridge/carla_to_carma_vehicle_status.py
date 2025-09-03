51c961235a7012f1faab436a104b8e48691d7440
west053 <167927749+west053@users.noreply.github.com>
2025-09-03 15:29:38 -0400

    gear fix. Now properly reflects proper vehicle gear when checking messages

--- carma-carla-integration/carma-carla-bridge/src/carma_carla_bridge/carla_to_carma_vehicle_status.py
+++ carma-carla-integration/carma-carla-bridge/src/carma_carla_bridge/carla_to_carma_vehicle_status.py
@@ -100,10 +100,18 @@ class CarlaToCarmaVehicleStatus(Node):
         status.speed = vehicle_status_msg.velocity
         
         # Set gear based on reverse control
-        if vehicle_status_msg.control.reverse:
-            status.current_gear.gear = Gear.REVERSE
-        else:
-            status.current_gear.gear = Gear.DRIVE
+        # Corrected code
+        carla_gear = vehicle_status_msg.control.gear
+
+        if carla_gear > 0:
+            # Any positive number from CARLA is Drive
+            status.current_gear.gear = 4 # DRIVE for VehicleStatus message
+        elif carla_gear == 0:
+            # Zero is Neutral
+            status.current_gear.gear = 2 # NEUTRAL for VehicleStatus message
+        else: # carla_gear < 0
+            # Any negative number from CARLA is Reverse
+            status.current_gear.gear = 1 # REVERSE for VehicleStatus message
         
         # Set drive mode based on manual gear shift
         if vehicle_status_msg.control.manual_gear_shift:
