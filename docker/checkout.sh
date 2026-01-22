05d1b6aba1aab1ffeabbb06ef041521a06b6abac
Kazawaryu <zexuan.jia@uga.edu>
2026-01-22 14:26:55 -0500

    update docker building checkout

--- docker/checkout.sh
+++ docker/checkout.sh
@@ -38,7 +38,8 @@ while [[ $# -gt 0 ]]; do
 done
 git clone https://github.com/usdot-fhwa-stol/carma-msgs.git "${dir}"/src/carma-msgs --branch  develop
 git clone https://github.com/usdot-fhwa-stol/carma-utils.git "${dir}"/src/carma-utils --branch  develop
-git clone https://github.com/Kazawaryu/carla-sensor-lib "${dir}"/src/carla-sensor-lib --branch  develop
+git clone https://github.com/usdot-fhwa-stol/carla-sensor-lib "${dir}"/src/carla-sensor-lib --branch  develop
+# TODO: carla-sensor-lib needs to merge PRs from Kazawaryu/carla-sensor-lib
 if [[ "${BRANCH}" == "develop" ]] || [[ "${BRANCH}" == "master" ]]; then
       git clone  https://github.com/usdot-fhwa-stol/autoware.ai.git "${dir}"/src/autoware.ai --branch carma-develop
 else
